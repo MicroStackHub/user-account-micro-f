b557bb078dcac94f138cb6d4157cb560e1d1fe1e
Shrishti Namdeo <shrishti.namdeo06@gmail.com>
2025-07-04 22:21:54 +0530

    Add Address

---
+++ address/src/App.tsx
@@ -0,0 +1,12 @@
+import AddressPage from './components/AddressPage'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <AddressPage />
+    </div>
+  )
+}
+
+export default App
